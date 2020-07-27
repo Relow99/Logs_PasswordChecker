2af1cdbad08f1b2f95116d4f60963c0de1905698
Tshwarelo <59955727+Relow99@users.noreply.github.com>
2020-07-27 22:14:01 +0200

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
-import java.lang.*;
 
 public class Main{
-    static Logger logger = LogManager.getLogger(PasswordChecker.class);
-
     public static void main (String[] args){
-            Scanner scanner = new Scanner (System.in);
-        logger.debug("please enter your password");
-            String password = scanner.nextLine();
-            try {
-                logger.error(PasswordChecker. passwordIsValid(password));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            logger.error(PasswordChecker.passwordIsOk(password));
+        String password;
+        Scanner passwordInput = new Scanner(System.in);
+        System.out.println("Please Enter Password");
+        password = passwordInput.nextLine();
+
+        try {
+            PasswordChecker.passwordIsValid(password);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
+        try {
+           PasswordChecker.passwordIsOk(password);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
     }
 
--- src/main/java/PasswordChecker.java
+++ src/main/java/PasswordChecker.java
@@ -1,64 +1,81 @@
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-import java.lang.*;
 
 public class PasswordChecker {
+    private static final Logger logger = LogManager.getLogger(PasswordChecker.class.getName());
+
     static String lowerCase = ".*[a-z].*";
     static String upperCase = ".*[A-Z].*";
     static String digit = ".*[0-9].*";
     static String specialChars = "(.*[^0-9,a-z,A-Z].*)";
-    static Logger logger = LogManager.getLogger(PasswordChecker.class);
 
-    public static String passwordIsValid(String password){
-
+    public static String passwordIsValid(String password) throws Exception {
         String results = "";
         if (password.isEmpty()) {
             results = "password should exist";
+            logger.error("- password should exist");
+//                throw new Exception("- password should exist");
 
         }else if (password.length() < 8) {
             results = "password should be longer than than 8 characters";
+            logger.error("- password should be longer than than 8 characters");
+//                    throw new Exception("- password should be longer than than 8 characters");
 
         }else if (!password.matches(lowerCase)) {
             results = "password should have at least one lowercase letter";
+            logger.error("- password should have at least one lowercase letter");
+//                        throw new Exception("- password should have at least one lowercase letter");
 
         }else if (!password.matches(upperCase)) {
-
             results = "password should have at least one uppercase letter";
+            logger.error("- password should have at least one uppercase letter");
+//          throw new Exception("- password should have at least one uppercase letter");
+
         }else if (!password.matches(digit)) {
-
             results = "password should at least have one digit";
-        }else if (!password.matches(specialChars)) {
+            logger.error("- password should at least have one digit");
+//          throw new Exception("- password should at least have one digit");
 
+        }else if (!password.matches(specialChars)) {
             results = "password should have at least one special character";
+            logger.error("- password should have at least one special character");
+//          throw new Exception("- password should have at least one special character");
+
         }
 
         return results;
     }
-    public static Boolean passwordIsOk (String password){
+    public static Boolean passwordIsOk (String password) throws Exception {
+
         int metConditions = 6;
         if (password.isEmpty()) {
             metConditions--;
+            logger.debug("User password is not ok");
+//            throw new Exception("the password is never OK if it doesn't exist and its" +
+//                        " less than 8 characters are not met.");
         }
         if (password.length() < 8) {
             metConditions--;
+            logger.debug("User password is not ok");
          }
-
         if (!password.matches(lowerCase)) {
             metConditions--;
-
+            logger.debug("User password is ok");
+//              throw new Exception("password is ok");
         }
         if (!password.matches(upperCase)) {
             metConditions--;
-
+            logger.debug("User password is ok");
         }
         if (!password.matches(digit)) {
-
             metConditions--;
+            logger.debug("User password is ok");
         }
         if (!password.matches(specialChars)) {
             metConditions--;
+            logger.debug("User password is ok");
         }
-//
+
         boolean passwordIsOk = false;
         if (metConditions >= 3) {
             passwordIsOk = true;
